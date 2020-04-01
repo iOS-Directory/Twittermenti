3dbaf0320708bd7b9f7e27b8abcad1ecfade48d4
fritzgt <tuanis@live.com>
2020-04-01 08:07:37 +0300

    initial commit

---
+++ Twittermenti/View Controllers/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  Twittermenti
+//
+//  Created by FGT MAC on 4/1/20.
+//  Copyright © 2020 FGT MAC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    
+    @IBOutlet weak var emojiLabel: UILabel!
+    @IBOutlet weak var textField: UITextField!
+    
+    
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    
+    @IBAction func predictButtonPRessed(_ sender: UIButton) {
+    }
+    
+    
+}
+
