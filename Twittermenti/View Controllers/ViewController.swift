1809b90a6a5b9606b786842e619c8179498cc3ef
fritzgt <tuanis@live.com>
2020-04-03 08:03:25 +0300

    working on request method

--- Twittermenti/View Controllers/ViewController.swift
+++ Twittermenti/View Controllers/ViewController.swift
@@ -16,15 +16,27 @@ class ViewController: UIViewController {
     @IBOutlet weak var textField: UITextField!
     
     
-    
+    // Instantiation using Twitter's OAuth Consumer Key and secret
+    let swifter = Swifter(consumerKey: "LTEeJyreyyuz4SWjVABuA1WKI", consumerSecret: "ajf107xfd6869heCG8Ptn8iztfFHBWPIxLkFGvsmoEwI7MKZcz")
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
     }
-
+    
     
     @IBAction func predictButtonPRessed(_ sender: UIButton) {
+        
+        if let searchQuery = textField.text {
+            
+            swifter.searchTweet(using: searchQuery, lang: "en", count: 100, tweetMode: .extended, success: { (results, metadata) in
+                print(results)
+            }) { (error) in
+                print(error)
+            }
+        }
+        
     }
     
     
